6e7ea3c7d4fcb8c592a84297bdf0bc2ddbb413f5
Lauren Laskerr <llask@me.com>
2019-09-08 17:36:17 -0400

    some changes to mobile etc, aparently it works on some mobile, but not my phone

--- js/src/data.js
+++ js/src/data.js
@@ -21,7 +21,7 @@ const weekday = [
   },
   {
     text: `It’s been a couple days, you kind of stink. Do you take a shower?`,
-    yes: 1,
+    yes: -1,
     no: 0,
     response: `default placeholder response`,
     background: "../../assets/backgrounds/bathroom.svg",
@@ -32,8 +32,8 @@ const weekday = [
   },
   {
     text: `Now that’s out of the way, it’s time to take your medication, do you make breakfast or take them with coffee?`,
-    yes: 1,
-    no: 2,
+    yes: -1,
+    no: -2,
     response: `default placeholder response`,
     background: "../../assets/backgrounds/kitchen.svg",
     buttonYes: "I make breakfast.",
@@ -43,7 +43,7 @@ const weekday = [
   },
   {
     text: `It’s 7:50, you’re going to be late, it’s time to get ready. Do you take your time or throw something on?`,
-    yes: 1,
+    yes: -1,
     no: 0,
     response: `default placeholder response`,
     background: "../../assets/backgrounds/bedroom.svg",
@@ -54,8 +54,8 @@ const weekday = [
   },
   {
     text: `It’s time to get to work, do you drive or take the train?`,
-    yes: 1,
-    no: 2,
+    yes: -1,
+    no: -2,
     response: `default placeholder response`,
     background: "../../assets/backgrounds/home.svg",
     buttonYes: "Driving.",
@@ -65,7 +65,7 @@ const weekday = [
   },
   {
     text: `You’ve arrived at work. It’s 8:30 am and you’re 30 minutes late. Do you jump right into working?`,
-    yes: 1,
+    yes: -1,
     no: 0,
     response: `default placeholder response`,
     background: "../../assets/backgrounds/work.svg",
@@ -77,7 +77,7 @@ const weekday = [
   {
     text: `It’s lunch time and your coworkers are heading off to lunch. You’re thinking about putting off lunch so you can catch up on work because you were late this morning. Do you eat lunch or do you skip it to catch up?`,
     yes: 0,
-    no: 1,
+    no: -1,
     response: `default placeholder response`,
     background: "../../assets/backgrounds/work.svg",
     buttonYes: "I've got to eat lunch.",
@@ -88,7 +88,7 @@ const weekday = [
   {
     text: `You’ve been working all day on your project for work, it seems like you’ve been at your computer all day, your body is getting stiff. Do you take a break, get up and stretch?`,
     yes: 0,
-    no: 1,
+    no: -1,
     response: `default placeholder response`,
     background: "../../assets/backgrounds/work.svg",
     buttonYes: "Take a break.",
@@ -98,7 +98,7 @@ const weekday = [
   },
   {
     text: `You’ve got a meeting. You go and participate, however afterwards you are asked some questions about your project. Do you stay?`,
-    yes: 1,
+    yes: -1,
     no: 0,
     response: `default placeholder response`,
     background: "../../assets/backgrounds/work.svg",
@@ -109,7 +109,7 @@ const weekday = [
   },
   {
     text: `You’re trying to grow your network and you’ve been invited to an event. If you go means that your evening routine will be thrown off and most likely you will get less sleep than normal. Do you go?`,
-    yes: 1,
+    yes: -1,
     no: 0,
     response: `default placeholder response`,
     background: "../../assets/backgrounds/work.svg",
@@ -120,8 +120,8 @@ const weekday = [
   },
   {
     text: `It’s been a long day, you’re hungry and tired. You look in the pantry and try to figure out what you can make for dinner. The thought of cooking and then having to clean up makes you feel even more tired. Do you cook yourself something to eat?`,
-    yes: 2,
-    no: 1,
+    yes: -2,
+    no: -1,
     response: `default placeholder response`,
     background: "../../assets/backgrounds/kitchen.svg",
     buttonYes: "I'll make myself something healthy.",
@@ -142,7 +142,7 @@ const weekday = [
   },
   {
     text: `You realize that you still have work left to do on your project for work. You look at the clock and you realize that it’s much later than you had thought. Do you push yourself to work on your project?`,
-    yes: 1,
+    yes: -1,
     no: 0,
     response: `default placeholder response`,
     background: "../../assets/backgrounds/work.svg",
@@ -153,8 +153,8 @@ const weekday = [
   },
   {
     text: `You’ve looked around and decided that it’s time to start your night time routine. You really should brush your teeth, wash your face and change into something for bed. Do you take care of your basic hygiene?`,
-    yes: 0,
-    no: 1,
+    yes: -1,
+    no: 0,
     response: `default placeholder response`,
     background: "../../assets/backgrounds/bathroom.svg",
     buttonYes: "Of course?",
@@ -171,8 +171,8 @@ const weekend = [
   {},
   {
     text: `Your alarm goes off, it’s 7:20 am. You crack your eyes open, you’re tired, you didn’t sleep well last night. Thankfully, it’s the weekend. Do you sleep in or get up?`,
-    yes: -1,
-    no: 1,
+    yes: 1,
+    no: -1,
     response: `default placeholder response`,
     background: "../../assets/backgrounds/bedroom.svg",
     buttonYes: "Sleep in!",
@@ -182,7 +182,7 @@ const weekend = [
   },
   {
     text: `So you’ve woken up, do you decide to take a shower today?`,
-    yes: 1,
+    yes: -1,
     no: 0,
     response: `default placeholder response`,
     background: "../../assets/backgrounds/bathroom.svg",
@@ -193,18 +193,18 @@ const weekend = [
   },
   {
     text: `Now that’s out of the way, it’s time to take your medication, do you make breakfast or take them with coffee?`,
-    yes: 1,
-    no: 2,
+    yes: -1,
+    no: -2,
     response: `default placeholder response`,
     background: "../../assets/backgrounds/kitchen.svg",
     buttonYes: "Gotta eat.",
-    buttonNo: "Give me the coffe.",
+    buttonNo: "Give me the coffee.",
     animate: "coffee pot",
     sound: null
   },
   {
     text: `You’ve been eating poorly recently, you’ve been thinking about doing meal prepping. Do you take time to go shopping for healthier food and prepare some food to eat?`,
-    yes: 2,
+    yes: -2,
     no: 0,
     response: `default placeholder response`,
     background: "../../assets/backgrounds/kitchen.svg",
@@ -215,7 +215,7 @@ const weekend = [
   },
   {
     text: `Seriously, you’ve been putting off your laundry for too long, it’s starting to pile up. Either you do it today, or it’s going to be more of a mess later.`,
-    yes: 1,
+    yes: -2,
     no: 0,
     response: `default placeholder response`,
     background: "../../assets/backgrounds/bedroom.svg",
@@ -226,7 +226,7 @@ const weekend = [
   },
   {
     text: `You look around and notice that you haven’t cleaned your place all week, do you pick up and clean?`,
-    yes: 2,
+    yes: -2,
     no: 0,
     response: `default placeholder response`,
     background: "../../assets/backgrounds/livingroom.svg",
@@ -237,7 +237,7 @@ const weekend = [
   },
   {
     text: `Your todo list is getting out of hand. Do you tackle it now?`,
-    yes: 2,
+    yes: -2,
     no: 0,
     response: `default placeholder response`,
     background: "../../assets/backgrounds/livingroom.svg",
@@ -248,8 +248,8 @@ const weekend = [
   },
   {
     text: `It’s lunch time and you’re tired. Do you eat lunch or have a nap?`,
-    yes: 1,
-    no: 1,
+    yes: -1,
+    no: -1,
     response: `default placeholder response`,
     background: "../../assets/backgrounds/livingroom.svg",
     buttonYes: "I need food!",
@@ -259,7 +259,7 @@ const weekend = [
   },
   {
     text: `You realize that you still have some projects to work on that are due on Monday, do you work on them?`,
-    yes: 1,
+    yes: -1,
     no: 0,
     response: `default placeholder response`,
     background: "../../assets/backgrounds/work.svg",
@@ -270,7 +270,7 @@ const weekend = [
   },
   {
     text: `Your phone buzzes, it’s your friends! They’re letting you know that they’re going out tonight, and are asking you what your plans are! It sounds like a good time, and you haven’t seen them in a while. Do you go?`,
-    yes: 2,
+    yes: -2,
     no: 0,
     response: `default placeholder response`,
     background: "../../assets/backgrounds/livingroom.svg",
@@ -282,7 +282,7 @@ const weekend = [
   {
     text: `It’s been a long day, you’re hungry and tired. You look in the pantry and try to figure out what you can make for dinner. The thought of cooking and then having to clean up makes you feel even more tired. Do you cook yourself something to eat?`,
     yes: 0,
-    no: 1,
+    no: -1,
     response: `default placeholder response`,
     background: "../../assets/backgrounds/kitchen.svg",
     buttonYes: "I meal prepped earlier.",
@@ -292,7 +292,7 @@ const weekend = [
   },
   {
     text: `You’ve looked around and decided that it’s time to start your night time routine. You really should brush your teeth, wash your face and change into something for bed. Do you take care of your basic hygiene?`,
-    yes: 1,
+    yes: -1,
     no: 0,
     response: `default placeholder response`,
     background: "../../assets/backgrounds/bathroom.svg",
--- js/src/levels.js
+++ js/src/levels.js
@@ -10,7 +10,6 @@ document.querySelector("#letsGo").onclick = () => {
   avatars(player1);
   levels(weekday, player1);
 }
-// document.querySelector("#letsGo").addEventListener("onclick" | "touchstart") = () => 
 // Player class
 class player {
   constructor(name, gender) {
@@ -192,8 +191,8 @@ function newDay(scenarios, player1, currentScene){
 }
 //handles everything that happens on button yes click
 function handleYes(scenarios, player1, currentScene) {
-  player1.spoons -= currentScene.yes
-  document.getElementById("response").innerHTML = `-${currentScene.yes} <i class="fa fa-utensil-spoon"></i>`
+  player1.spoons += currentScene.yes
+  document.getElementById("response").innerHTML = `${currentScene.yes} <i class="fa fa-utensil-spoon"></i>`
   document.getElementById("response").setAttribute("style", "opacity: 1")
   gameScore.innerHTML = `<p>Spoons: ${player1.spoons}</p><p>Day: ${player1.day}</p>`
   setAvi(player1);
@@ -202,8 +201,8 @@ function handleYes(scenarios, player1, currentScene) {
 }
 //handles everything that happens on button no click
 function handleNo(scenarios, player1, currentScene) {
-  player1.spoons -= currentScene.no
-  document.getElementById("response").innerHTML = `-${currentScene.no} <i class="fa fa-utensil-spoon"></i>`
+  player1.spoons += currentScene.no
+  document.getElementById("response").innerHTML = `${currentScene.no} <i class="fa fa-utensil-spoon"></i>`
   document.getElementById("response").setAttribute("style", "opacity: 1")
   gameScore.innerHTML = `<p>Spoons: ${player1.spoons}</p><p>Day: ${player1.day}</p>`
   setAvi(player1);
@@ -223,15 +222,14 @@ function newScene(scenarios, player1, currentScene) {
 function endResult(player1) {
   //win
   if (player1.spoons > 0) {
-    alert("you made it through another week.")
     document.getElementById("final").innerHTML = `You made it through another week.<br> Your final spoon count was ${player1.spoons}.<br><br>
     If this game helped you understand what it's like to live with chronic illness better, please <style>.bmc-button img{width: 27px !important;margin-bottom: 1px !important;box-shadow: none !important;border: none !important;vertical-align: middle !important;}.bmc-button{line-height: 36px !important;height:37px !important;text-decoration: none !important;display:inline-flex !important;color:#ffffff !important;background-color:#000000 !important;border-radius: 3px !important;border: 1px solid transparent !important;padding: 0px 9px !important;font-size: 17px !important;letter-spacing:-0.08px !important;box-shadow: 0px 1px 2px rgba(190, 190, 190, 0.5) !important;-webkit-box-shadow: 0px 1px 2px 2px rgba(190, 190, 190, 0.5) !important;margin: 0 auto !important;font-family:'Lato', sans-serif !important;-webkit-box-sizing: border-box !important;box-sizing: border-box !important;-o-transition: 0.3s all linear !important;-webkit-transition: 0.3s all linear !important;-moz-transition: 0.3s all linear !important;-ms-transition: 0.3s all linear !important;transition: 0.3s all linear !important;}.bmc-button:hover, .bmc-button:active, .bmc-button:focus {-webkit-box-shadow: 0px 1px 2px 2px rgba(190, 190, 190, 0.5) !important;text-decoration: none !important;box-shadow: 0px 1px 2px 2px rgba(190, 190, 190, 0.5) !important;opacity: 0.85 !important;color:#ffffff !important;}</style><link href="https://fonts.googleapis.com/css?family=Lato&subset=latin,latin-ext" rel="stylesheet"><a class="bmc-button" target="_blank" href="https://www.buymeacoffee.com/justlask"><img src="https://bmc-cdn.nyc3.digitaloceanspaces.com/BMC-button-images/BMC-btn-logo.svg" alt="Buy me a coffee"><span style="margin-left:5px">Buy me a coffee</span></a>`
-    document.getElementById("final").setAttribute("style", "opacity: 1")
+    document.getElementById("final").setAttribute("style", "display: block")
   }
   // "lose"
   else {
     document.getElementById("final").innerHTML = `You were not careful with your spoons and over exerted yourself. <br>Doing this could cause you to have a flare up.<br> Your final spoon count was ${player1.spoons}.<br><br>
     If this game helped you understand what it's like to live with chronic illness better, please <style>.bmc-button img{width: 27px !important;margin-bottom: 1px !important;box-shadow: none !important;border: none !important;vertical-align: middle !important;}.bmc-button{line-height: 36px !important;height:37px !important;text-decoration: none !important;display:inline-flex !important;color:#ffffff !important;background-color:#000000 !important;border-radius: 3px !important;border: 1px solid transparent !important;padding: 0px 9px !important;font-size: 17px !important;letter-spacing:-0.08px !important;box-shadow: 0px 1px 2px rgba(190, 190, 190, 0.5) !important;-webkit-box-shadow: 0px 1px 2px 2px rgba(190, 190, 190, 0.5) !important;margin: 0 auto !important;font-family:'Lato', sans-serif !important;-webkit-box-sizing: border-box !important;box-sizing: border-box !important;-o-transition: 0.3s all linear !important;-webkit-transition: 0.3s all linear !important;-moz-transition: 0.3s all linear !important;-ms-transition: 0.3s all linear !important;transition: 0.3s all linear !important;}.bmc-button:hover, .bmc-button:active, .bmc-button:focus {-webkit-box-shadow: 0px 1px 2px 2px rgba(190, 190, 190, 0.5) !important;text-decoration: none !important;box-shadow: 0px 1px 2px 2px rgba(190, 190, 190, 0.5) !important;opacity: 0.85 !important;color:#ffffff !important;}</style><link href="https://fonts.googleapis.com/css?family=Lato&subset=latin,latin-ext" rel="stylesheet"><a class="bmc-button" target="_blank" href="https://www.buymeacoffee.com/justlask"><img src="https://bmc-cdn.nyc3.digitaloceanspaces.com/BMC-button-images/BMC-btn-logo.svg" alt="Buy me a coffee"><span style="margin-left:5px">Buy me a coffee</span></a>`
-    document.getElementById("final").setAttribute("style", "opacity: 1")
+    document.getElementById("final").setAttribute("style", "display: block")
   }
 }
